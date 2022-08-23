c3acc931ad12fc372fbdd0bea1f40f8a19b3d4d4
chingleung <chingleung@wafersystems.com>
2022-08-23 17:54:21 +0800

    8月23日公司学习

---
+++ demo-1/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app-container">
+    <h1>App 根组件</h1>
+    <hr />
+
+    <div class="box">
+      <!-- 渲染 Left 组件和 Right 组件 -->
+      <!-- 3. 以标签形式，使用注册好的组件 -->
+      <Left></Left>
+      <Right></Right>
+    </div>
+  </div>
+</template>
+
+<script>
+// 1. 导入需要使用的 .vue 组件
+import Left from '@/components/Left.vue';
+import Right from '@/components/Right.vue';
+
+export default {
+  // 2. 注册组件
+  components: {
+    // 组件名称:要注册的组件
+    // 'Left': Left // 全写形式
+    // 在对象里面 键和值 一样可以简写，如下：
+    Left,
+    "Right": Right
+ }
+}
+</script>
+
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #efefef;
+}
+.box {
+  display: flex;
+}
+</style>
+
